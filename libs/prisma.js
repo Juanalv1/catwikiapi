4db98897f3aa2d5894a8a264bc3227cd6c86b6f5
Juan <elquesififa@gmail.com>
2024-02-15 22:10:56 -0500

    aaaa

--- libs/prisma.js
+++ libs/prisma.js
@@ -1,3 +1,8 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-module.exports = prisma;
+try {
+  const { PrismaClient } = require('@prisma/client');
+  const prisma = new PrismaClient();
+  module.exports = prisma;
+} catch (error) {
+  console.error('Error initializing Prisma:', error);
+  process.exit(1);
+}
