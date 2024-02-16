3b60961a5833ef67f7e11692a527d63bbca6ab3d
Juan <elquesififa@gmail.com>
2024-02-15 22:09:30 -0500

    aaaa

--- models/breed.js
+++ models/breed.js
@@ -1,6 +1,4 @@
-const { PrismaClient } = require('@prisma/client');
-
-const prisma = new PrismaClient();
+const prisma = require('../libs/prisma')
 
 class BreedModel {
   static async getMany({ onlyNames, limit, top }) {
